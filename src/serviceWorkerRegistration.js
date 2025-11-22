b94f8e7bb379d880207bf034faaaa8722cfd06f2
ankitsharma38 <ankitsharma7805@gmail.com>
2025-11-22 13:17:34 +0530

    PWA enabled with service worker and manifest

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,14 @@
+export function registerSW() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(() => {
+          console.log('Service Worker registered');
+        })
+        .catch(error => {
+          console.log('Service Worker registration failed:', error);
+        });
+    });
+  }
+}
